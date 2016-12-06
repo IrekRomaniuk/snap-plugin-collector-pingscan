3611fb79c5478b88f57e7857cd4001af6331874a
irom77 <irek.romaniuk@gmail.com>
2016-12-05 20:33:31 -0500

    Testing continue

--- pingscan/pingscan_test.go
+++ pingscan/pingscan_test.go
@@ -83,7 +83,7 @@ func TestReadTargets(t *testing.T) {
 	target := "../examples/pinglist.txt"
 		hosts, _ := ReadTargets(target)
 		Convey("So pinglist.txt should contain 3 items", func() {
-			So(len(hosts), ShouldEqual,1100)
+			So(len(hosts), ShouldEqual,1110)
 		})
 	})
 }
