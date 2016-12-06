ef40199b08cbab29bd7e71ac4c39c03fa770d713
irom77 <irek.romaniuk@gmail.com>
2016-12-05 20:36:31 -0500

    Testing continue

--- pingscan/scan/scan.go
+++ pingscan/scan/scan.go
@@ -3,7 +3,6 @@ package scan
 import (
 	"os/exec"
 	"github.com/IrekRomaniuk/snap-plugin-collector-pingscan/pingscan/targets"
-	"fmt"
 )
 
 func Ping(hosts []string) int {
@@ -25,7 +24,7 @@ func Ping(hosts []string) int {
 	alives := <-doneChan
 	result := targets.DeleteEmpty(alives)
 
-	fmt.Printf("\n%d/%d %d\n", len(result),len(hosts),concurrentMax)
+	//fmt.Printf("\n%d/%d %d\n", len(result),len(hosts),concurrentMax)
 	return len(result)
 }
 
